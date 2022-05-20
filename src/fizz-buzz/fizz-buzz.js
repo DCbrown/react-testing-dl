b7247546808b1fa89d4e2a3d7345843af04710c0
dbrown-dl <donovan.brown@detroitlabs.com>
2022-05-20 14:28:39 -0400

    lesson 2

--- src/fizz-buzz/fizz-buzz.js
+++ src/fizz-buzz/fizz-buzz.js
@@ -1,4 +1,12 @@
 const fizzBuzz = (value) => {
+  if (value % 3 === 0) {
+    return "Fizz";
+  }
+
+  if (value % 5 === 0) {
+    return "Buzz";
+  }
+
   return value;
 };
 
--- src/fizz-buzz/fizz-buzz.test.js
+++ src/fizz-buzz/fizz-buzz.test.js
@@ -1,10 +1,10 @@
 import fizzBuzz from "./fizz-buzz";
 
 describe("fizz-buzz", () => {
-  it("should exist - basic test setup", () => {
-    // Arrange
-    const subject = fizzBuzz;
+  // Arrange
+  const subject = fizzBuzz;
 
+  it("should exist - basic test setup", () => {
     // Act
     // const result = subject();
 
@@ -12,33 +12,22 @@ describe("fizz-buzz", () => {
     expect(subject).toBeDefined();
   });
   it("should return 1 when passed 1", () => {
-    // Arrange
-    const subject = fizzBuzz;
-
     // Act
     const result = subject(1);
 
     // Assert
     expect(result).toBe(1);
   });
-  it("should return 2 when passed 2", () => {
-    // Arrange
-    const subject = fizzBuzz;
-
-    // Act
-    const result = subject(2);
-
-    // Assert
-    expect(result).toBe(2);
-  });
   it("should return Fizz when passed 3", () => {
-    // Arrange
-    const subject = fizzBuzz;
-
-    // Act
-    const result = subject(3);
-
-    // Assert
-    expect(result).toBe("Fizz");
+    expect(subject(3)).toBe("Fizz");
+  });
+  it("should return Buzz when passed 5", () => {
+    expect(subject(5)).toBe("Buzz");
+  });
+  it("should return Fizz when passed 6", () => {
+    expect(subject(6)).toBe("Fizz");
+  });
+  it("should return Buzz when passed 10", () => {
+    expect(subject(10)).toBe("Buzz");
   });
 });
